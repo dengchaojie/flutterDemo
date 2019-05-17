2665bd88f7fad28e3ab95cca7f7e3d4a2382b03e
dengchaojie <dengchaojiedcj@163.com>
2019-05-17 21:16:55 +0800

    movie_top_item

---
+++ flutter_douban/lib/movie/movie_top_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_douban/movie/movie_list_item.dart';
+
+class MovieTopItem extends StatelessWidget
+{
+
+  final int index;
+  final MovieListItem item;
+  const MovieTopItem(this.index, this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
+
--- flutter_douban/lib/movie/movie_top_list_view.dart
+++ flutter_douban/lib/movie/movie_top_list_view.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_douban/model/movie_item.dart';
+import 'package:flutter_douban/public.dart';
+import 'dart:ui' as ui;
+import 'movie_top_item.dart';
+import 'movie_list_item.dart';
+import 'package:flutter/cupertino.dart';
 
 class MovieTopListView extends StatefulWidget {
 
@@ -14,10 +21,123 @@ class MovieTopListView extends StatefulWidget {
 }
 
 class MovieTopListViewState extends State<MovieTopListView> {
+  List<MovieItem> movieList;
+  double coverWidth = Screen.width;
+  double coverHeight = 218 + Screen.topSafeHeight;
+  int start = 0;
+  int count = 25;
+  bool _loaded = false;
+  bool isVisible = true;
+  double navAlpha = 0;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Expanded(
+                  child: ListView(
+                      children: <Widget>[
+                        _buildHeader(),
+                        _buildList()
+                      ],
+                  )
+
+              )
+            ],
+          )
+
+        ],
+      ),
+    );
   }
+
+//  Widget _buildNavgationBar() {
+//    return
+//
+//  }
+
+  Widget _buildList() {
+    int index = 1;
+    List<Widget> children = [];
+    for(int i = 0; i < movieList.length; i++ ) {
+      children.add(MovieTopItem(index++, MovieListItem(movieList[i], this.widget.action)));
+    }
+    Widget loading = Container(
+      padding: EdgeInsets.symmetric(vertical: 15),
+      child: Offstage(
+        offstage: _loaded,
+        child: CupertinoActivityIndicator(),
+      ),
+    );
+    children.add(loading);
+    return Container(
+      child: Column(
+        children: children,
+      ),
+    );
+
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Image(
+              image: CachedNetworkImageProvider(movieList[0].images.large),
+              width: coverWidth,
+              height: coverHeight,
+              fit: BoxFit.fitWidth,
+
+          ),
+          Container(
+            color: Color(0xbb000000),
+            width: coverWidth,
+            height: coverHeight,
+
+          ),
+          BackdropFilter(
+            filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              width: coverWidth,
+              height: coverHeight,
+              padding: EdgeInsets.fromLTRB(30, 54 + Screen.topSafeHeight, 10, 20),
+              color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    this.widget.subTitle,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    this.widget.title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+
+            ),
+
+          ),
+
+        ],
+      ),
+    );
+
+
+
+  }
+
 }
